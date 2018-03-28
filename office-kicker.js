f5e7b962154def1b76970c14817275c7e85d3b9f
M Nasimul Haque <nasim.haque@appliansys.com>
2018-03-28 09:24:13 +0100

    un-rotate the doc after kicking

--- office-kicker.js
+++ office-kicker.js
@@ -9,13 +9,20 @@ var reactions = [
     "Umph"
 ];
 
+var show_message = function(msg) {
+    return function() {
+        var res = alert(msg);
+        document.querySelector('body').style['transform'] = 'rotate(0deg)';
+    };
+};
+
 var kick = function() {
     var reaction = reactions[Math.floor(Math.random() * reactions.length)];
     var deg = 20 - 45 * Math.random();
     document.getElementsByTagName('body')[0].style['transform'] = 'rotate(' + deg + 'deg)';
     var ow_msg = "Office says: \"" + reaction + "!\"";
-    setTimeout("alert('" + ow_msg + "');", 0.1);
-}
+    setTimeout(show_message(ow_msg), 0.1);
+};
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
